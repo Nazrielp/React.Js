cb050d9e83416f83ebe8e31dcbfb15089079ec6c
Nazrielp19 <149786827+Nazrielp19@users.noreply.github.com>
2024-02-25 14:42:15 +0700

    Button

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+// class Button extends React.Component {
+//   render() {
+//     return (
+//       <button className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white" type="submit">
+//           Buy now
+//         </button> 
+//     );
+//   }
+// }
+
+// function ButtonBlack () {
+//   return (
+//     <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+//     Buy now
+//   </button> 
+
+//   );
+// }
+
+const Button = (props) => {
+  const { children = "...", variant = "bg-black" } = props;
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
+      {children}
+    </button>
+
+  );
+}
+
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-600 min-h-screen items-center">
+      <div className="flex gap-x-3">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-slate-700">Logout</Button>
+        <Button></Button>
+      </div>
+    </div>
+  );
+}
+
+
+export default App
