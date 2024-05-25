f4b30b55831314bf3f17aadefa1e979b77580d98
Nazrielp19 <149786827+Nazrielp19@users.noreply.github.com>
2024-05-25 14:02:41 +0700

    Login

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
     <div className="flex justify-center min-h-screen items-center">
       <div className="w-full max-w-xs">
         <h1 className="text-3xl font-bold mb-2 text-blue-600">Login</h1>
-        <p className="font-medium text-slate-500 mb-8">Welcome, please enter your details</p>
+        <p className="font-medium text-slate-500 mb-8">Welcome, please enter your details broo</p>
 
         <from action="">
           <div className="mb-6">
