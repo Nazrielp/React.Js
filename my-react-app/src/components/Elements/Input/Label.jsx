a27387767396b477d8aeda1c8aa0acbd041a93ca
Nazrielp19 <149786827+Nazrielp19@users.noreply.github.com>
2024-07-01 14:35:42 +0700

    Login

--- my-react-app/src/components/Elements/Input/Label.jsx
+++ my-react-app/src/components/Elements/Input/Label.jsx
@@ -1,12 +1,11 @@
 const Label = (props) => {
-    const { htmlFor, label} = props
+    const { htmlFor, Children} = props
     return (
         <label
             htmlFor={htmlFor}
             className="block text-slate-700 text-sm font-bold mb-2">
-            {label}
+            {Children}
         </label>
-
     );
 };
 
