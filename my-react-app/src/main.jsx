6138df3288a4dc695f95553500e713b337702b78
Nazrielp19 <149786827+Nazrielp19@users.noreply.github.com>
2024-07-05 13:29:35 +0700

    Root Login and Register

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from './Pages/LoginPage.jsx';
+import RegisterPage from './Pages/RegisterPage.jsx';
+
+const router = createBrowserRouter ([
+  {
+    path: "/",
+    element: <div className="flex justify-center min-h-screen items-center">Hello Word!</div>,
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage></RegisterPage>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
