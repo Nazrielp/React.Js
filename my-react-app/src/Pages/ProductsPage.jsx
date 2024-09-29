d5a756e8c28d6131dfd494ac504a175b2f802b36
Nazrielp19 <149786827+Nazrielp19@users.noreply.github.com>
2024-09-29 11:34:32 +0700

    Rnedering List

--- my-react-app/src/Pages/ProductsPage.jsx
+++ my-react-app/src/Pages/ProductsPage.jsx
@@ -1,19 +1,42 @@
 import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragment/CardProduct";
 
+const product = [
+    {
+        id: 1,
+        name: "Sepatu Baru",
+        price: "1.100.000",
+        image: "/images/shoes-1.jpg",
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique veniam quam incidunt tempora voluptatem ipsum, laboriosam rerum magnam asperiores debitis?',
+    },
+    {
+        id: 2,
+        name: "Sepatu Lama",
+        price: "700.000",
+        image: "/images/image.png",
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+        id: 3,
+        name: "Sepatu Kece",
+        price: "1.500.000",
+        image: "/images/image-2.jpg",
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique veniam quam incidunt tempora voluptatem ipsum.',
+    }
+]
+
 const ProductsPage = () => {
     return (
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes-1.jpg" />
-                <CardProduct.Body title="Sepatu Sneakers">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique veniam quam incidunt tempora voluptatem ipsum, laboriosam rerum magnam asperiores debitis?</CardProduct.Body>
-                <CardProduct.Footer price="Rp. 100.000"></CardProduct.Footer>
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image="/images/shoes-1.jpg" />
-                <CardProduct.Body title="Sepatu Sneakers">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique veniam quam incidunt tempora voluptatem ipsum, laboriosam rerum magnam asperiores debitis?</CardProduct.Body>
-                <CardProduct.Footer price="Rp. 1.000.000.000"></CardProduct.Footer>
-            </CardProduct>
+
+            {product.map((product) => (
+                <CardProduct>
+                    <CardProduct.Header image={product.image} />
+                    <CardProduct.Body name={product.name}>{product.description}</CardProduct.Body>
+                    <CardProduct.Footer price={product.price}></CardProduct.Footer>
+                </CardProduct>
+            ))}
+
         </div>
 
     );
--- my-react-app/src/components/Fragment/CardProduct.jsx
+++ my-react-app/src/components/Fragment/CardProduct.jsx
@@ -2,7 +2,7 @@ import Button from "../Elements/Button";
 
 const CardProduct = ({ children }) => {
     return (
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2">
+        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between">
             {children}
         </div>
     );
@@ -16,11 +16,11 @@ const Header = ({ image }) => {
     );
 };
 
-const Body = ({ children, title }) => {
+const Body = ({ children, name }) => {
     return (
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 h-full">
             <a href="">
-                <h5 className="text-xl font-semibold tracking-tight text-green-500">{title}</h5>
+                <h5 className="text-xl font-semibold tracking-tight text-green-500">{name}</h5>
             </a>
             <p className="text-s text-white">
                 {children}
